f535a8bf57490a34eda2fb36c7d3e0c411377037
Swamim Saikia <swamimsaikia18@gmail.com>
2020-06-21 11:36:25 +0530

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MaterialApp(
+      home: Center(
+    child: Row(
+      children: <Widget>[
+        Text('hello 1'),
+        Text('hello 2'),
+        Text('hello 3'),
+        Text('hello Joon'),
+      ],
+    ),
+  )));
+}
