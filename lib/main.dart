2d32f5dd20820c1e03eb77047f6e25977affa740
Swamim Saikia <swamimsaikia18@gmail.com>
2020-06-21 12:46:13 +0530

    Second commit Swamim

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,29 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new MaterialApp(
-      home: Center(
-    child: Row(
-      children: <Widget>[
-        Text('hello 1'),
-        Text('hello 2'),
-        Text('hello 3'),
-        Text('hello Joon'),
-      ],
-    ),
-  )));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: HomePage()));
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Shopping App'),
+        backgroundColor: Colors.purple,
+        actions: <Widget>[
+          new IconButton(
+              icon: Icon(Icons.search, color: Colors.white), onPressed: null),
+          new IconButton(
+              icon: Icon(Icons.shopping_cart, color: Colors.white),
+              onPressed: null)
+        ],
+      ),
+    );
+  }
 }
