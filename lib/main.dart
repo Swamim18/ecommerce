146ea715bff275c46efa2510920f479f5150df1c
Swamim Saikia <swamimsaikia18@gmail.com>
2020-06-21 13:32:23 +0530

    Third commit Swamim

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,83 @@ class _HomePageState extends State<HomePage> {
               onPressed: null)
         ],
       ),
+      drawer: new Drawer(
+          child: new ListView(
+        children: <Widget>[
+          //header
+          new UserAccountsDrawerHeader(
+            accountName: Text('Swamim Saikia'),
+            accountEmail: Text('swamim@mail.com'),
+            currentAccountPicture: GestureDetector(
+                child: new CircleAvatar(
+              backgroundColor: Colors.white,
+              child: Icon(
+                Icons.person,
+                color: Colors.grey,
+              ),
+            )),
+            decoration: new BoxDecoration(color: Colors.purple),
+          ),
+          InkWell(
+            onTap: () {},
+            child: ListTile(
+              title: Text('Home'),
+              leading: Icon(Icons.home),
+            ),
+          ),
+
+          InkWell(
+            onTap: () {},
+            child: ListTile(
+              title: Text('My Account'),
+              leading: Icon(Icons.person),
+            ),
+          ),
+
+          InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('My Orders'),
+                leading: Icon(
+                  Icons.shopping_basket,
+                ),
+              )),
+
+          InkWell(
+            onTap: () {},
+            child: ListTile(
+              title: Text('Catagories'),
+              leading: Icon(Icons.dashboard),
+            ),
+          ),
+
+          InkWell(
+            onTap: () {},
+            child: ListTile(
+              title: Text('Favourites'),
+              leading: Icon(Icons.favorite),
+            ),
+          ),
+
+          Divider(),
+
+          InkWell(
+            onTap: () {},
+            child: ListTile(
+              title: Text('Settings'),
+              leading: Icon(Icons.settings),
+            ),
+          ),
+
+          InkWell(
+            onTap: () {},
+            child: ListTile(
+              title: Text('About'),
+              leading: Icon(Icons.help),
+            ),
+          )
+        ],
+      )),
     );
   }
 }
