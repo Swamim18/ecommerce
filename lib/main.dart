3b5d123f268268fb3c3e956f6ffebc996cbf8dcb
Swamim Saikia <swamimsaikia18@gmail.com>
2020-06-24 16:10:11 +0530

    Carousel Swamim

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_pro/carousel_pro.dart';
 
 void main() {
   runApp(MaterialApp(debugShowCheckedModeBanner: false, home: HomePage()));
@@ -12,8 +13,24 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    Widget image_carousel = new Container(
+        height: 200.0,
+        child: new Carousel(
+          boxFit: BoxFit.cover,
+          images: [
+            AssetImage('images/c1.jpg'),
+            AssetImage('images/m1.jpeg'),
+            AssetImage('images/m2.jpeg'),
+            AssetImage('images/w1.jpeg'),
+            AssetImage('images/w3.jpeg'),
+            AssetImage('images/w4.jpeg'),
+          ],
+          autoplay: false,
+        ));
+
     return Scaffold(
       appBar: AppBar(
+        elevation: 0.1,
         title: Text('Shopping App'),
         backgroundColor: Colors.purple,
         actions: <Widget>[
